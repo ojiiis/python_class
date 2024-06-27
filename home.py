ea3dbfec5ba6b18a996cd3141724145d721cf36b
ojiiis <official.ojingirisamuel@gmail.com>
2024-06-27 15:39:33 +0100

    abc

--- home.py
+++ home.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask,request
+user = "Samuel"
+def login_form():
+    return """
+<p>Welcome {0}, to our python home page</p>
+<form>
+<input type="text"><br><br>
+<input type="text"><br><br>
+<input type="button"><br><br>
+</form>
+""".format(user)
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/",methods=['GET', 'POST'])
 def home():
-    return "Home page"
+    if request.method == "POST":
+        return "d"
+    else:
+        return login_form()
+
+
+@app.route("/profile")
+def profile():
+    return "profile page"
